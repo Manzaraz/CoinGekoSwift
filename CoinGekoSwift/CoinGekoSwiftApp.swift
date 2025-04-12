c3b6b0e88c5b81781cc30670c3003a4ba996dd41
Christian Manzaraz <magnusmanz@icloud.com>
2025-04-12 19:54:15 -0300

    Initial Commit

---
+++ CoinGekoSwift/CoinGekoSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinGekoSwiftApp.swift
+//  CoinGekoSwift
+//
+//  Created by Christian Manzaraz on 12/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct CoinGekoSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
